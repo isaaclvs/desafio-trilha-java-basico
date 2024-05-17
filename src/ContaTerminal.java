7710e7d973a11efba2a96b00909c6acd0f2d6edb
isaaclvs <isaacfelipee3@gmail.com>
2024-05-17 08:02:45 -0300

    Finalizado...

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -4,27 +4,24 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in); // Instanciando a ferramenta Scanner
 
-        // Declarando as variáveis
-        int numeroDaConta;
-        String agencia;
-        String nomeDoCliente;
-        double saldo;
-
         // Recebendo os dados do cliente
         System.out.println("Digite o número da conta: ");
-        numeroDaConta = scanner.nextInt();
+        int numeroDaConta = scanner.nextInt();
 
         System.out.println("Digite o número da agência: ");
-        agencia = scanner.next();
+        String agencia = scanner.next();
 
         System.out.println("Digite o seu nome: ");
-        nomeDoCliente = scanner.next();
+        String nomeDoCliente = scanner.next();
+
+        System.out.println("Digite o seu sobrenome: ");
+        String sobrenomeDoCliente = scanner.next();
 
         System.out.println("Insira o saldo:");
-        saldo = scanner.nextDouble();
+        double saldo = scanner.nextDouble();
 
         // Retornando os dados formatados
 
-        System.out.println("Olá " + nomeDoCliente + ", obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + ", conta: " + numeroDaConta + " e seu saldo: R$" + saldo + " já está disponível para saque.");
+        System.out.println("Olá " + nomeDoCliente.concat(" ").concat(sobrenomeDoCliente) + ", obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + ", conta: " + numeroDaConta + " e seu saldo: R$" + saldo + " já está disponível para saque.");
     }
 }
